2c8ba5918f463a98bf551e1c0536b1e05e3b1909
Prabhudutt Dash <dash.prabhuduttpd1@gmail.com>
2023-09-11 07:52:02 +0530

    Update file 19.c Co-authored-by: Prince Kumar <robertprincekumar@gmail.com>

---
+++ 19.c
@@ -0,0 +1,15 @@
+/*Objective: To find if a given integer is even or odd */
+#include<stdio.h>
+void main()
+{
+   //Declaration
+   int a;
+   //Input
+   printf("Enter a no.");
+   scanf("%d",&a);
+   //Logic
+   if(a%2==0)
+    printf("\n a is even");
+   else
+    printf("\n a is odd");
+}
