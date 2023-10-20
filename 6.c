489b1a71f7a30acda6b3f74bc77a89e931cb5ce6
Prabhudutt Dash <dash.prabhuduttpd1@gmail.com>
2023-10-21 05:27:42 +0530

    Update file 6.c Co-authored-by: Prince Kumar <robertprincekumar@gmail.com>

---
+++ 6.c
@@ -0,0 +1,10 @@
+/*Find the areas and volume of square*/
+#include<stdio.h>
+void main()
+{
+  float a,area;
+  printf("Enter the side of square");
+  scanf("%f",&a);
+  area=a*a;
+  printf("/n Area of square is %f",area);
+}
