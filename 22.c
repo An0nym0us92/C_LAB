3e2c4985ea5936c2375977e9460babc59ba08162
An0nym0us92 <dash.prabhuduttpd1@gmail.com>
2025-08-18 07:05:24 +0530

    Update file 22.c Co-authored-by: Prince Kumar <robertprincekumar@gmail.com>

---
+++ 22.c
@@ -0,0 +1,12 @@
+/*Objective : To compute square of a no. */
+#include<stdio.h>
+void main()
+{
+  int a;
+  printf("Enter a no.");
+  scanf("%d",&a);
+  int b=a*a;
+  printf("\n Square of a +:",a);
+
+
+}
